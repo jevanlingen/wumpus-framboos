d421a3321952cdf9aac82d577c763285c238ceb5
lingenj <je.lingen@portofrotterdam.com>
2024-06-11 21:02:43 +0200

    Add `score` to Competition for easier display in frontend

--- backend/src/main/kotlin/framboos/vrolijke/jdriven/com/dao/impl/CompetitionRepositoryImpl.kt
+++ backend/src/main/kotlin/framboos/vrolijke/jdriven/com/dao/impl/CompetitionRepositoryImpl.kt
@@ -8,14 +8,24 @@ import org.jetbrains.exposed.sql.ResultRow
 class CompetitionRepositoryImpl : ReadRepositoryImpl<Competition>(Competitions), CompetitionRepository {
     override fun toDto(row: ResultRow) = Competition(
         id = row[Competitions.id].value,
-        currentGame = row[Competitions.currentGameId],
-        games = row[Competitions.gameIds],
+        currentGameId = row[Competitions.currentGameId],
+        gameIds = row[Competitions.gameIds],
     )
 
     override suspend fun allIds() = dbQuery {
         Competitions
             .select(Competitions.id)
             .map { it[Competitions.id].value }
+    }
+
+    override suspend fun findById(id: Int): Competition? {
+        val competition = super.findById(id)
+        val score = (competition?.gameIds ?: emptyList())
+            .flatMap { g -> playerRepo.findByGameId(g).map { Score(it.userId, it.user, it.points) } }
+            .groupBy { it.userId }
+            .map { (userId, scoreList) -> Score(userId, scoreList.first().username, scoreList.sumOf { it.points }) }
+
+        return competition?.copy(score = score)
     }
 }
 
--- backend/src/main/kotlin/framboos/vrolijke/jdriven/com/dao/model/Competition.kt
+++ backend/src/main/kotlin/framboos/vrolijke/jdriven/com/dao/model/Competition.kt
@@ -2,7 +2,8 @@ package framboos.vrolijke.jdriven.com.dao.model
 
 import org.jetbrains.exposed.dao.id.IntIdTable
 
-data class Competition(override val id: Int, val currentGame: Int = -1, val games: List<Int> = emptyList()) : Dto
+data class Score(val userId: Int, val username: String, val points: Int)
+data class Competition(override val id: Int, val currentGameId: Int = -1, val gameIds: List<Int> = emptyList(), val score: List<Score> = emptyList()) : Dto
 
 object Competitions : IntIdTable() {
     val currentGameId = integer("current_game_id").uniqueIndex()
